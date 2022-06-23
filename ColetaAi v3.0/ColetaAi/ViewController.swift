778c26d6e2227a9a213e8e4506a6079f4ab30c0f
prompty001 <43974118+prompty001@users.noreply.github.com>
2022-06-22 21:30:46 -0300

    Add files via upload

---
+++ ColetaAi v3.0/ColetaAi/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  ColetaAi
+//
+//  Created by student on 04/07/19.
+//  Copyright © 2019 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController{
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
---
+++ ColetaAi v3.0/ColetaAi/reciclarViewController.swift
@@ -0,0 +1,24 @@
+//
+//  reciclarViewController.swift
+//  ColetaAi
+//
+//  Created by student on 08/07/19.
+//  Copyright © 2019 student. All rights reserved.
+//
+
+import UIKit
+
+class reciclarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
